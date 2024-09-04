d5ee1697b91ed773f1ff64416f423a7d71f32127
Sheymor <joseacvz81@gmail.com>
2024-09-04 11:09:06 -0400

    Add initial project setup with configuration files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.Views.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.menu();
+    }
+}
